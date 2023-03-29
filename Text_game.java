39eb8187d3540cfb40c4e42a94cef3b622b9b64a
kaufmha <kaufmha@whs.school.nz>
2023-03-30 11:25:44 +1300

    Version 4 Added values for rooms in an array.
    Signed-off-by: kaufmha <kaufmha@whs.school.nz>

--- Text_game.java
+++ Text_game.java
@@ -10,7 +10,6 @@ import java.util.Scanner; //To be able to use scanners.
 
 public class Text_game
 {
-    // instance variables - replace the example below with your own
     //private String[] rooms  
     /** Starting the game.
      *  Have fun.
@@ -31,15 +30,26 @@ public class Text_game
         //Instructions = "The controlls are West,North,South,East,Down,Up,Look/examine room";
         Scanner looking = new Scanner(System.in);
         System.out.println("Controlls to the game are West,North,South,East,Down,Up");
-        
+        int North []= {0,0,0,0,0};
+        int West []= {0,0,0,0,0};
+        int East []= {0,0,0,0,0};
+        int South []= {0,0,0,0,0};
+        int Up []= {0,0,0,0,0};
+        int Down []= {0,0,0,0,0};
         //goal= "Your goal is to collect 3 items so you can leave the building"; //The goal of the game
         System.out.print(goal); //Prints out the goal string.
-        int[] rooms = new int[8];//Array for rooms
-        rooms[1]=2;
-        rooms[2]=2;
-        
+        int[] room = new int[8];//Array for rooms
+        room[0]=0;
+        room[1]=1;
+        room[2]=2;
+        room[3]=3;
+        room[4]=4;
+        room[5]=5;
+        room[6]=6;
+        room[7]=7;
         
         if (look = true){// Hmmm 
+            
             System.out.println("look is true");// For checking if it is working
         
         
